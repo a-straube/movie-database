a5ae33b83b7e9e9179acea0f7f8d7eab67300d38
Franz Knupfer & Axel Straube <student@epicodus.com>
2015-05-06 12:11:20 -0700

    Add all specs and methods for Actor class, except update and delete method

--- lib/actor.rb
+++ lib/actor.rb
@@ -5,4 +5,31 @@ class Actor
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    returned_actors = DB.exec("SELECT * FROM actors;")
+    actors = []
+    returned_actors.each() do |actor|
+      name = actor.fetch("name")
+      id = actor.fetch("id").to_i()
+      actors.push(Actor.new({:name => name, :id => id}))
+    end
+    actors
+  end
+
+  define_method(:==) do |actor2|
+    self.name().==(actor2.name()).&(self.id().==(actor2.id()))
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO actors (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_singleton_method(:find) do |id|
+    @id = id
+    returned_actor = DB.exec("SELECT * FROM actors WHERE id = #{@id};")
+    @name = returned_actor.first().fetch("name")
+    Actor.new({:name => @name, :id => @id})
+  end
 end
--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -15,4 +15,36 @@ describe(Actor) do
       expect(actor.id()).to(eq(1))
     end
   end
+
+  describe('.all') do
+    it("has no actors in it at first") do
+      expect(Actor.all()).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it("if it has the same name and same id sets to equal") do
+      actor1 = Actor.new({:name => "Brad Pitt", :id => nil})
+      actor2 = Actor.new({:name => "Brad Pitt", :id => nil})
+      expect(actor1).to(eq(actor2))
+    end
+  end
+
+  describe('#save') do
+    it("saves an actor to the database") do
+      actor = Actor.new({:name => "Ingar Bergman", :id => nil})
+      actor.save()
+      expect(Actor.all()).to(eq([actor]))
+    end
+  end
+
+  describe('.find') do
+    it("finds the actor for a specific id") do
+      actor = Actor.new({:name => "Brad Pitt", :id => nil})
+      actor.save()
+      actor2 = Actor.new({:name => "John Smith", :id => nil})
+      actor2.save()
+      expect(Actor.find(actor2.id())).to(eq(actor2))
+    end
+  end
 end
